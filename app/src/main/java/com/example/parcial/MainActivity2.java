2f3ee264c88988242511f7f3ece02e7ff970180a
Italo Guevara <alberto.villamil.1997@gmail.com>
2024-02-23 20:02:16 -0500

    TITLE= ventanas android

---
+++ app/src/main/java/com/example/parcial/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.parcial;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.parcial.ui.main.MainFragment;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
